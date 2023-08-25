5f7b11969c9acd8c8f4a7cbb457c99bc535969e6
mohdnawaz6393@gmail.com <mohdnawaz6393@gmail.com>
2023-08-25 11:54:01 +0500

    adding meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ const App = () => {
         />
 
         <Route path='/about' element={<Layout children={<About />} />} />
+
         <Route path='*' element={<Layout children={<Landing />} />} />
       </Routes>
     </MyProvider>
